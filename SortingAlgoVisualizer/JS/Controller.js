351c805d868e373afbc45799f73933cf17d4b128
Celeste Artley <90732656+CelesteGiraffe@users.noreply.github.com>
2024-09-20 21:44:25 -0500

    Updated the sorting algos and made sure all worked and on mobile

--- SortingAlgoVisualizer/JS/Controller.js
+++ SortingAlgoVisualizer/JS/Controller.js
@@ -1,49 +1,59 @@
 import { AlgoSort } from "./AlgoSort.js";
+
 var algoSort = new AlgoSort(120, "AlgoDisplay", 650);
 var algoSortSmall = new AlgoSort(27, "sAlgoDisplay", 395);
-//Series of event listeners for the buttons under the canvas
+
+// Event listeners for the main canvas
 document
-    .getElementById("reset")
-    .addEventListener("click", algoSort.reset.bind(algoSort));
+  .getElementById("reset")
+  .addEventListener("click", algoSort.reset.bind(algoSort));
 document
-    .getElementById("newArray")
-    .addEventListener("click", algoSort.newArray.bind(algoSort));
+  .getElementById("newArray")
+  .addEventListener("click", algoSort.newArray.bind(algoSort));
 document
-    .getElementById("quick")
-    .addEventListener("click", algoSort.quickSort.bind(algoSort));
+  .getElementById("quick")
+  .addEventListener("click", algoSort.quickSort.bind(algoSort));
 document
-    .getElementById("bubble")
-    .addEventListener("click", algoSort.bubbleSort.bind(algoSort));
+  .getElementById("bubble")
+  .addEventListener("click", algoSort.bubbleSort.bind(algoSort));
 document
-    .getElementById("insertion")
-    .addEventListener("click", algoSort.insertionSort.bind(algoSort));
+  .getElementById("insertion")
+  .addEventListener("click", algoSort.insertionSort.bind(algoSort));
 document
-    .getElementById("heap")
-    .addEventListener("click", algoSort.heapSort.bind(algoSort));
+  .getElementById("heap")
+  .addEventListener("click", algoSort.heapSort.bind(algoSort));
 document
-    .getElementById("test")
-    .addEventListener("click", algoSort.functioningSortTestAlgo.bind(algoSort));
-//end series of Event listeners for buttons under canvas.
-//Series of event listeners for the buttons under the canvas
+  .getElementById("merge")
+  .addEventListener("click", algoSort.mergeSort.bind(algoSort));
 document
-    .getElementById("sreset")
-    .addEventListener("click", algoSortSmall.reset.bind(algoSortSmall));
+  .getElementById("test")
+  .addEventListener("click", algoSort.functioningSortTestAlgo.bind(algoSort));
+
+// Event listeners for the small canvas
 document
-    .getElementById("snewArray")
-    .addEventListener("click", algoSortSmall.newArray.bind(algoSortSmall));
+  .getElementById("sreset")
+  .addEventListener("click", algoSortSmall.reset.bind(algoSortSmall));
 document
-    .getElementById("squick")
-    .addEventListener("click", algoSortSmall.quickSort.bind(algoSortSmall));
+  .getElementById("snewArray")
+  .addEventListener("click", algoSortSmall.newArray.bind(algoSortSmall));
 document
-    .getElementById("sbubble")
-    .addEventListener("click", algoSortSmall.bubbleSort.bind(algoSortSmall));
+  .getElementById("squick")
+  .addEventListener("click", algoSortSmall.quickSort.bind(algoSortSmall));
 document
-    .getElementById("sinsertion")
-    .addEventListener("click", algoSortSmall.insertionSort.bind(algoSortSmall));
+  .getElementById("sbubble")
+  .addEventListener("click", algoSortSmall.bubbleSort.bind(algoSortSmall));
 document
-    .getElementById("sheap")
-    .addEventListener("click", algoSortSmall.heapSort.bind(algoSortSmall));
+  .getElementById("sinsertion")
+  .addEventListener("click", algoSortSmall.insertionSort.bind(algoSortSmall));
 document
-    .getElementById("stest")
-    .addEventListener("click", algoSortSmall.functioningSortTestAlgo.bind(algoSortSmall));
-//end series of Event listeners for buttons under canvas.
+  .getElementById("sheap")
+  .addEventListener("click", algoSortSmall.heapSort.bind(algoSortSmall));
+document
+  .getElementById("smerge")
+  .addEventListener("click", algoSortSmall.mergeSort.bind(algoSortSmall));
+document
+  .getElementById("stest")
+  .addEventListener(
+    "click",
+    algoSortSmall.functioningSortTestAlgo.bind(algoSortSmall)
+  );
--- SortingAlgoVisualizer/JS/SortingUtils.js
+++ SortingAlgoVisualizer/JS/SortingUtils.js
@@ -1,63 +1,46 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-//this class is to provide usefull utilities to the AlgoSort algorithm such as Swaping, Waiting, and Recursion.
 export class SortingUtils {
-    constructor(c) {
-        this.sleep = () => {
-            return new Promise((resolve) => setTimeout(resolve, SortingUtils.waitTime));
-        };
-        this.c = c;
+  constructor(c) {
+    this.c = c;
+  }
+
+  sleep() {
+    return new Promise((resolve) => setTimeout(resolve, SortingUtils.waitTime));
+  }
+
+  swap(leftIndex, rightIndex, items) {
+    var temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
+  }
+
+  async quickSortRecursion(array, start, end) {
+    if (start >= end) {
+      return;
     }
-    swap(leftIndex, rightIndex, items) {
-        var temp = items[leftIndex];
-        items[leftIndex] = items[rightIndex];
-        items[rightIndex] = temp;
+    let index = await this.quickSortPartition(array, start, end);
+    await Promise.all([
+      this.quickSortRecursion(array, start, index - 1),
+      this.quickSortRecursion(array, index + 1, end),
+    ]);
+  }
+
+  async quickSortPartition(array, start, end) {
+    var pivotIndex = start;
+    var pivotValue = array[end];
+    for (let i = start; i < end; i++) {
+      if (array[i] < pivotValue) {
+        this.swap(i, pivotIndex, array);
+        await this.sleep();
+        this.c.clear();
+        this.c.drawGraph(array);
+        pivotIndex++;
+      }
     }
-    quickSortRecursion(array, start, end) {
-        return __awaiter(this, void 0, void 0, function* () {
-            //Get the next split index to decide where to recursivly run the function next time.
-            //recursively call the partition based on the index collected.
-            if (start >= end) {
-                return;
-            }
-            let index = yield this.quickSortPartition(array, start, end);
-            yield Promise.all([
-                this.quickSortRecursion(array, start, index - 1),
-                this.quickSortRecursion(array, index + 1, end),
-            ]);
-        });
-    }
-    quickSortPartition(array, start, end) {
-        return __awaiter(this, void 0, void 0, function* () {
-            //Determine a pivot point and the index, this can be done in a few ways, however, the easiest is to just assign the last in the array.
-            //Create a for loop over all numbers from the start to end of the passed pointers
-            //check for values and put the larger to the right of the pivot and leave the ones to the left.
-            //after swaping clear and draw the canvas.
-            //then at the end of the for loop take the index and put it back in the array from the end.
-            var pivotIndex = start;
-            var pivotValue = array[end];
-            for (let i = start; i < end; i++) {
-                if (array[i] < pivotValue) {
-                    this.swap(i, pivotIndex, array);
-                    yield this.sleep();
-                    this.c.clear();
-                    this.c.drawGraph(array);
-                    pivotIndex++;
-                }
-            }
-            this.swap(pivotIndex, end, array);
-            this.c.clear();
-            this.c.drawGraph(array);
-            return pivotIndex;
-        });
-    }
+    this.swap(pivotIndex, end, array);
+    this.c.clear();
+    this.c.drawGraph(array);
+    return pivotIndex;
+  }
 }
-//used to set the delay between frames during sorting.
+
 SortingUtils.waitTime = 5;
